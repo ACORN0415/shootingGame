1cede5dd3043f40c2879b8d7699a7328ebf02b3b
jun <acorn0415@naver.com>
2022-09-23 14:30:36 +0900

    shootingGame

---
+++ Scripts/EnemyManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    float currentTime;
+    public float createTIme =1;
+    public GameObject enemyFactory;
+    float minTime = 1;
+    float maxTime = 5;
+    void Start()
+    {
+        createTIme = UnityEngine.Random.Range(minTime, maxTime);    
+    }
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        if(currentTime>createTIme)
+        {
+            GameObject enemy = Instantiate(enemyFactory);
+            enemy.transform.position = transform.position;
+            currentTime = 0;
+            createTIme = UnityEngine.Random.Range(minTime, maxTime);
+        }
+    }
+}
---
+++ Scripts/PlayerMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 5;
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 dir = new Vector3(h, v, 0);
+
+        transform.position += dir * speed * Time.deltaTime;
+    }
+}
